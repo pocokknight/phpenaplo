8fc2a215e3146527b1e92dd7d095608ef8d1cb1b
pocokknight <hortobiadam@gmail.com>
2017-03-29 21:08:01 +0200

    0

---
+++ 0evek.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pocok : E-napló</title>
+        <link href="css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <form name="aform" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            <div class="hatter">
+                <a name="gomb" class="gomb" value="gomb1" id="gomb1">Évek</a>
+                <a name="gomb" class="gomb" value="gomb2" id="gomb2">Új év</a>
+                <div class="helyzetsav">
+                    <div class="ev"></div>
+                    <div class="osztaly"></div>
+                    <div class="diak"></div>
+                    <div class="targy"></div>
+                </div>
+                <div class="lista"><?php 
+                    include './connect.php';
+                    $sql = "select ev from ev";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $str = $row["ev"];
+                            echo " <a href=\"1osztalyok.php\">$str</a></br>";
+                        }
+                    }
+                    ?></div>
+            </div>
+        </form>
+    </body>
+</html>
